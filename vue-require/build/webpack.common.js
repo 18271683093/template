a4fc63473c935f8be56fb5a4abdb1b7e892e4c44
unknown <18271683093>
2019-07-28 22:54:23 +0800

    保存-7-28

--- vue-require/build/webpack.common.js
+++ vue-require/build/webpack.common.js
@@ -1,14 +1,15 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './app/index.js'
+    first: path.resolve(__dirname, "../src/assets/main.js")
   },
   plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin()
+    new CleanWebpackPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
--- vue-require/build/webpack.dev.js
+++ vue-require/build/webpack.dev.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -11,18 +12,18 @@ module.exports = merge(common, {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: 'first.html',//打包好后，新建的html名字为first.html
-      template: './src/index.html',//以src下面的index.html为模板去创建新的html文件
-      title: "test"
+      filename: 'first/first.html', //打包好后，新建的html名字为first.html
+      template: './src/index.html', //以src下面的index.html为模板去创建新的html文件
+      title: "test",
+      path: "first",
+
     }),
   ],
   mode: "development",
   module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+    rules: [{
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
   }
 })
